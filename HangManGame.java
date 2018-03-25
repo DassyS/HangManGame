9c72563300ad26004d5a68e7566b10fb87eace0d
DassyS <37508795+DassyS@users.noreply.github.com>
2018-03-25 21:25:24 +0300

    Update HangManGame.java

--- HangManGame.java
+++ HangManGame.java
@@ -13,8 +13,11 @@ public class HangManGame {
 	private int score;
 	private String randomWord;
 	private int numTries;
+	static int tries;
 	public static List<Player> players =new ArrayList <Player>();
+	static HashSet<Character> lettersIN;
 	static Scanner stdIn = new Scanner(System.in);
+	static int ID=0;
 
 	public void setID(int ID) {
 		this.gameID = ID;
@@ -81,6 +84,7 @@ public class HangManGame {
 		return data.get(index);
 	}
 	//this method prints a char array
+
 	public static void printArray(char[]word)
 	{
 		for(char c: word){
@@ -89,27 +93,29 @@ public class HangManGame {
 		System.out.print("\n");
 	}
 	//this method checks how many times a letter appears in a string and enters it in to the * array for the view of the player
+
 	public static int findLetter(String word, char letter, char[] enteredLetters)
 	{
-		int tries=0;
+		boolean flag=false;
 		int index = word.indexOf(letter);
-		HashSet<Character> lettersIN = new HashSet<Character>();
-		lettersIN.add(letter);
-			if (lettersIN.contains(letter))
-			{
-				System.out.print("The letter "+letter+" was already entered, please try again");
-			}
-			else
-			{
-				while (index >= 0) {
+		if (lettersIN.contains(letter))
+		{
+			System.out.print("The letter "+letter+" was already entered, please try again\n");
+		}
+		else
+		{
+			lettersIN.add(letter);
+			tries++;
+			//System.out.print(tries);
+			while (index >= 0) {
+				flag=true;
 				enteredLetters[index]=letter;
-				System.out.println(index);
+				//System.out.println(index);
 				index = word.indexOf(letter, index + 1);
-				tries++;
 			}
-				if (index==(-1)){
-					System.out.println("The letter "+letter+" is not in the word");
-				}
+			if (flag==false){
+				System.out.println("The letter "+letter+" is not in the word");
+			}
 		}
 		return tries;
 	}
@@ -126,7 +132,7 @@ public class HangManGame {
 	private static int calculateScore(int tries, String word) {
 		return (((word.length())/tries)*10);
 	}
-	
+
 	private static int calculateMissed(String word,char[] enteredLetters) {
 		int index=0;
 		int missed=0;
@@ -137,18 +143,16 @@ public class HangManGame {
 		}
 		return missed;
 	}
-	
+
 	public static HangManGame playGame(String user)
 	{
-		int ID=0;
-		int tries=0;
 		//set up new game
-		
+
 		HangManGame g = new HangManGame();
 		ID++; //set game number
 		g.setID(ID);
 		g.setPlayer(user);
-		String word=getRandomWord("words.txt"); //get random word from dic.
+		String word=getRandomWord("words.txt"); //get random word from dict.
 		//System.out.print(word+"\n");
 		g.setWord(word);
 		char[] enteredLetters = new char[word.length()]; //create array for hidden word.
@@ -157,86 +161,83 @@ public class HangManGame {
 		for (int i=0; i<word.length();i++){
 			enteredLetters[i]='*';
 		}
-		printArray(enteredLetters); //print hidden word.
+		printArray(enteredLetters); //prints string with * and guessed letters.
 		boolean wordIsGuessed=false;
-		for (int i=0; i<((word.length())*2); i++) //loop will run double the amount of letters in words.
+		int i=0;
+		while (i<(word.length())&& (wordIsGuessed==false))
 		{
-			while (wordIsGuessed==false)
+			i++;
+			char letter= (stdIn.next()).charAt(0); //get entered letter
+			if (Character.isLetter(letter)) //check if letter char.
 			{
-
-				char letter= (stdIn.next()).charAt(0); //get entered letter
-				if (Character.isLetter(letter)) //check if letter char.
-				{
-					//System.out.print(letter);
-					wordIsGuessed=checkWord(enteredLetters); //check if letter in the word. flag will turn true if all letters in words will get guessed.
-					if (wordIsGuessed==true){
-						break;
-					}
-					tries=findLetter(word,letter,enteredLetters); //look for index of letter in word. if exist then added to array. counts the tries.
-					printArray(enteredLetters); //prints the array again after each guess.
-				}
-				else
-				{
-					continue;
+				//System.out.print(letter);
+				tries=findLetter(word,letter,enteredLetters); //look for index of letter in word. if exist then added to array. counts the tries.
+				printArray(enteredLetters); //prints the array again after each guess.
+				wordIsGuessed=checkWord(enteredLetters); //check if letter in the word. flag will turn true if all letters in words will get guessed.
+				if (wordIsGuessed==true){
+					break;
 				}
 			}
 		}
-		if (wordIsGuessed=true){
-			g.setScore(calculateScore(tries, word));
-			System.out.print("Congragulations! You have guessed the word: "+word+" in "+tries+" tries.\n Your score is: "+g.getScore());
+	if (wordIsGuessed=true){
+		g.setScore(calculateScore(tries, word));
+		System.out.print("Congragulations! You have guessed the word: "+word+" in "+tries+" tries.\n Your score is: "+g.getScore()+"\n");
+	}
+	else{
+		int missed=calculateMissed(word, enteredLetters);
+		g.setScore(tries+missed);
+		System.out.print("Sorry! not this time. The word was "+word+"and you had "+missed+ "misses. Your score is "+g.getScore()+"\n");
+	}
+	g.setTries(tries);
+	return g;
+}
+public static void checkPlayer(String user, String email){
+	//iterate through list of players, if player exist pull if not create player
+	for (Player player:players){
+		if (player.getUser()==user && player.getEmail()==email){
+			System.out.print("Hello, "+user+" \n");
+		}
+		if (player.getUser()==user && player.getEmail()!=email){
+			System.out.print("Email address is not correct.\n ");
+		}
+		if (player.getUser()!=user && player.getEmail()==email){
+			System.out.print("User name is not correct\n ");
 		}
 		else{
-			int missed=calculateMissed(word, enteredLetters);
-			g.setScore(tries+missed);
-			System.out.print("Sorry! not this time. The word was "+word+"and you had "+missed+ "misses. Your score is "+g.getScore());
-		}
-		g.setTries(tries);
-		return g;
-	}
-	public static void checkPlayer(String user, String email){
-		//iterate through list of players, if player exist pull if not create player
-		for (Player player:players){
-			if (player.getUser()==user && player.getEmail()==email){
-				System.out.print("Hello, "+user);
-			}
-			if (player.getUser()==user && player.getEmail()!=email){
-				System.out.print("Email address is not correct. ");
-			}
-			if (player.getUser()!=user && player.getEmail()==email){
-				System.out.print("User name is not correct ");
-			}
-			else{
-				Player p = new Player(user, email);
-				System.out.print("Hello, "+p.getUser());
-			}
+			Player p = new Player(user, email);
+			System.out.print("Hello, "+p.getUser());
 		}
 	}
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Plase enter a user name: ");
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	int choice=0;
+	while (choice!=1){
+		System.out.print("Please enter a user name: ");
 		String user= stdIn.next();
-		System.out.print("Plase enter an Email Address: ");
+		System.out.print("Please enter an Email Address: ");
 		String email= stdIn.next();
 		checkPlayer(user,email);
-		System.out.print("Welcome to hang-man-game, Please enter your chioce:/n"
-				+ "1-exit"
-				+ "2-list all my games"
-				+ "3-start a new game");
-		int choice=stdIn.nextInt();
+		System.out.print("Welcome to hang-man-game, Please enter your chioce:\n"
+				+ "1-exit\n"
+				+ "2-list all my games\n"
+				+ "3-start a new game\n");
+		choice=stdIn.nextInt();
 		if (choice==2){
 			for (HangManGame game:Player.games){
 				System.out.print(game.gameID+"\t"+game.score+"\n");
 			}
 		}
 		if (choice==3){
+			tries=0;
+			lettersIN = new HashSet<Character>();
 			HangManGame game=playGame(user);
 			Player.games.add(game);
 		}
-		
-		stdIn.close();
 	}
-	
+
+	stdIn.close();
 }
 
-
+}
